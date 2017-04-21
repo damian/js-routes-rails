0eb552a3d796a0fedc51381969ef7e1167fca18f
Emil Sågfors <emil.sagfors@iki.fi>
2017-04-21 11:04:40 +0300

    Extract output stubbing in route exporter spec

--- spec/lib/js-routes/rails/route_exporter_spec.rb
+++ spec/lib/js-routes/rails/route_exporter_spec.rb
@@ -2,16 +2,18 @@ describe Js::Routes::Rails::RouteExporter do
   let(:routes) { Rails.application.routes.routes }
   let(:finder) { Js::Routes::Rails::RouteFinder.new(routes) }
   subject { described_class.new(finder) }
+  let(:fake_output) { StringIO.new }
+
+  before do
+    output_path = Js::Routes::Rails.configuration.output
+    allow(File).to receive(:open).with(output_path, "w").and_yield(fake_output)
+  end
 
   it "should store a reference to the exportable routes" do
     expect(subject.routes).to eq(finder.routes)
   end
 
   it "should generate a JavaScript file lib containing the routes and output it on disk" do
-    fake_output = StringIO.new
-    output_path = Js::Routes::Rails.configuration.output
-    allow(File).to receive(:open).with(output_path, "w").and_yield(fake_output)
-
     subject.export!
 
     subject.routes.each do |helper, path|
@@ -33,10 +35,6 @@ describe Js::Routes::Rails::RouteExporter do
     it "should render the custom template" do
       fake_template = "Hello world. 1 + 2 = <%= 1 + 2 %>"
       allow(File).to receive(:read).with(template_path).and_return(fake_template)
-
-      fake_output = StringIO.new
-      output_path = Js::Routes::Rails.configuration.output
-      allow(File).to receive(:open).with(output_path, "w").and_yield(fake_output)
 
       subject.export!
       expect(fake_output.string).to eq("Hello world. 1 + 2 = 3")
